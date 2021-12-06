b27a48fa05fd9d8bec5877c53d3963b7c74d1177
LucasBenvenuti <lucasbenvenuti2010@gmail.com>
2021-12-06 10:56:45 -0800

    Test 2

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "Hello"
-    var content: String = "There"
+    var content: String = "There New Test"
     
     var id: String = UUID().uuidString
     
